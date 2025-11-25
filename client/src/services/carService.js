c5abb0e93f83edaf91db8c74e4f3d589aa089911
Muhammad Maaz Aleem <muhammadmaazaleem@gmail.com>
2025-11-25 13:54:54 -0800

    Fix car creation - remove multipart form data requirement

--- client/src/services/carService.js
+++ client/src/services/carService.js
@@ -15,21 +15,13 @@ export const getCar = async (id) => {
 
 // Create car (admin)
 export const createCar = async (carData) => {
-  const response = await api.post('/cars', carData, {
-    headers: {
-      'Content-Type': 'multipart/form-data'
-    }
-  });
+  const response = await api.post('/cars', carData);
   return response.data;
 };
 
 // Update car (admin)
 export const updateCar = async (id, carData) => {
-  const response = await api.put(`/cars/${id}`, carData, {
-    headers: {
-      'Content-Type': 'multipart/form-data'
-    }
-  });
+  const response = await api.put(`/cars/${id}`, carData);
   return response.data;
 };
 
